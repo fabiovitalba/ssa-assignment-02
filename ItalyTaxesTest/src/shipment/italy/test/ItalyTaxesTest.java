36ce47dc0233a82b2e5036626acc0cda9e13d3ed
Fabio.Vitalba <fabvit9@gmail.com>
2024-04-07 16:22:30 +0200

    Implemented ItalyTaxesTest

--- ItalyTaxesTest/src/shipment/italy/test/ItalyTaxesTest.java
+++ ItalyTaxesTest/src/shipment/italy/test/ItalyTaxesTest.java
@@ -8,10 +8,58 @@ import shipment.italy.ItalyTaxes;
 
 public class ItalyTaxesTest {
     @Test
-    public void calculateFoodTaxTest() {
-        Shipment s = new Shipment("apple","FOOD",20,1.0,00.0,"Italy");
+    public void calculateDefaultTaxTest() {
+        int itemQuantity = 20;
+        double unitPrice = 2.50;
+        double shipmentCost = 100.0;
+        double expectedTax = 30.0;
+
+        Shipment s = new Shipment("pasta","FOOD",itemQuantity,unitPrice,shipmentCost,"Italy");
         ItalyTaxes it = new ItalyTaxes();
-        double calculatedTax = it.calculateTax(s);
-        assertEquals((20 * 0.3),calculatedTax,0.001);
+        double calculatedTaxAmount = it.calculateTax(s);
+        double expectedTaxAmount = (itemQuantity * unitPrice) * (expectedTax / 100);
+        assertEquals(expectedTaxAmount,calculatedTaxAmount,0.001);
+    }
+
+    @Test
+    public void calculateHighIndividualPriceTaxTest() {
+        int itemQuantity = 20;
+        double unitPrice = 2500;
+        double shipmentCost = 100.0;
+        double expectedTax = 30.0;
+
+        Shipment s = new Shipment("wine","ALCOHOL",itemQuantity,unitPrice,shipmentCost,"Italy");
+        ItalyTaxes it = new ItalyTaxes();
+        double calculatedTaxAmount = it.calculateTax(s);
+        double expectedTaxAmount = (itemQuantity * unitPrice) * (expectedTax / 100) + 5;
+        assertEquals(expectedTaxAmount,calculatedTaxAmount,0.001);
+    }
+
+    @Test
+    public void calculateClothesTaxTest() {
+        int itemQuantity = 100;
+        double unitPrice = 499;
+        double shipmentCost = 100.0;
+        double expectedTax = 30.0;
+
+        Shipment s = new Shipment("dress","CLOTHES",itemQuantity,unitPrice,shipmentCost,"Italy");
+        ItalyTaxes it = new ItalyTaxes();
+        double calculatedTaxAmount = it.calculateTax(s);
+        double expectedTaxAmount = (itemQuantity * unitPrice) * (expectedTax / 100) * 0.8;
+        assertEquals(expectedTaxAmount,calculatedTaxAmount,0.001);
+    }
+
+    @Test
+    public void calculateClothesHighAmountTaxTest() {
+        int itemQuantity = 100;
+        double unitPrice = 501;
+        double shipmentCost = 100.0;
+        double expectedTax = 30.0;
+
+        Shipment s = new Shipment("dress","CLOTHES",itemQuantity,unitPrice,shipmentCost,"Italy");
+        ItalyTaxes it = new ItalyTaxes();
+        double calculatedTaxAmount = it.calculateTax(s);
+        double expectedTaxAmount = (itemQuantity * unitPrice) * (expectedTax / 100) + 5;
+        assertEquals(expectedTaxAmount,calculatedTaxAmount,0.001);
     }
 }
