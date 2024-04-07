132c7366fe46630d50501b2ef98498c80fed40c6
Fabio.Vitalba <fabvit9@gmail.com>
2024-04-07 17:57:18 +0200

    Fixed ShipmentFileProcessorTest

--- MainTest/src/shipment/main/test/ShipmentFileProcessorTest.java
+++ MainTest/src/shipment/main/test/ShipmentFileProcessorTest.java
@@ -25,32 +25,32 @@ public class ShipmentFileProcessorTest {
         csvShipments1.createNewFile();
 
         FileWriter fw = new FileWriter(csvFile1);
-        fw.write("coffee;FOOD;1000;5;500;Brazil;");
-        fw.write("cellphone;ELETRONICS;70;800;1500;China;");
-        fw.write("board game;TOY;2000;100;150;Italy;");
+        fw.write("coffee;FOOD;1000;5;500;Brazil;\n");
+        fw.write("cellphone;ELETRONICS;70;800;1500;China;\n");
+        fw.write("board game;TOY;2000;100;150;Italy;\n");
         fw.close();
 
         File xmlShipments1 = new File(xmlFile1);
         xmlShipments1.createNewFile();
         fw = new FileWriter(xmlFile1);
-        fw.write("<shipments>");
-        fw.write("<shipment product=\"coffee\" type=\"FOOD\" amount=\"1000\" individualprice=\"5\" shipmentPrice=\"500\" country=\"Brazil\" />");
-        fw.write("</shipments>");
+        fw.write("<shipments>\n");
+        fw.write("<shipment product=\"coffee\" type=\"FOOD\" amount=\"1000\" individualprice=\"5\" shipmentPrice=\"500\" country=\"Brazil\" />\n");
+        fw.write("</shipments>\n");
         fw.close();
 
         File xmlShipments2 = new File(xmlFile2);
         xmlShipments2.createNewFile();
         fw = new FileWriter(xmlFile2);
-        fw.write("<shipments>");
-        fw.write("</shipments>");
+        fw.write("<shipments>\n");
+        fw.write("</shipments>\n");
         fw.close();
 
         File txtShipments1 = new File(txtFile1);
         txtShipments1.createNewFile();
         fw = new FileWriter(txtFile1);
-        fw.write("coffee;FOOD;1000;5;500;Brazil;");
-        fw.write("cellphone;ELETRONICS;70;800;1500;China;");
-        fw.write("board game;TOY;3000;100;2000;China;");
+        fw.write("coffee;FOOD;1000;5;500;Brazil;\n");
+        fw.write("cellphone;ELETRONICS;70;800;1500;China;\n");
+        fw.write("board game;TOY;3000;100;2000;China;\n");
         fw.close();
     }
 
@@ -71,7 +71,7 @@ public class ShipmentFileProcessorTest {
 
     @Test
     public void calcCsvExample() {
-        double expectedTaxAmountOwed = 1.0;
+        double expectedTaxAmountOwed = 79000.0;
         double taxAmountOwed = ShipmentFileProcessor.calculate(csvFile1);
         assertEquals(expectedTaxAmountOwed,taxAmountOwed,0.001);
     }
